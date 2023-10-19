6cbaa6379d0e1024c7fece78fad8276fde13cd9a
TharinduDR <rhtdranasinghe@gmail.com>
2023-10-19 12:17:59 +0100

    002: data cleaning

--- experiments/clean.py
+++ experiments/clean.py
@@ -1,5 +1,6 @@
 import os
 import json
+import codecs
 
 def merge_json_files(root_dir, output_file):
     merged_data = []
@@ -9,18 +10,18 @@ def merge_json_files(root_dir, output_file):
         for filename in filenames:
             if filename.endswith('.json'):
                 file_path = os.path.join(foldername, filename)
-                with open(file_path, 'r',encoding="utf8") as json_file:
+                with codecs.open(file_path, 'r', encoding='utf-8') as json_file:
                     try:
                         data = json.load(json_file)
                         merged_data.append(data)
                     except json.JSONDecodeError:
                         print(f"Error reading file: {file_path}")
 
-    with open(output_file, 'w', encoding='utf8') as output_json:
-        json.dump(merged_data, output_json)
+    with codecs.open(output_file, 'w', encoding='utf-8') as output_json:
+        json.dump(merged_data, output_json, ensure_ascii=False)
 
 if __name__ == "__main__":
     root_directory = "data/"  # Change this to the directory where your JSON files are located
-    output_filename = "NSINa.json"  # Change this to your desired output filename
+    output_filename = "merged.json"  # Change this to your desired output filename
 
     merge_json_files(root_directory, output_filename)
