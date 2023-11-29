8808b9b63dd77edca8035d1daf777fef262358ef
TharinduDR <rhtdranasinghe@gmail.com>
2023-11-29 09:27:24 +0000

    001: Initial commit

--- experiments/t5_experiment.py
+++ experiments/t5_experiment.py
@@ -10,7 +10,7 @@ from config.model_args import T5Args
 from t5.t5_model import T5Model
 from transformer_model.evaluation import bleu, ter
 
-model_name = "GermanT5/t5-base-german-3e"
+model_name = "sinhala-nlp/sin-t5-oscar"
 model_type = "t5"
 
 
@@ -22,7 +22,7 @@ full = full.rename(columns={'News Content': 'input_text', 'Headline': 'target_te
 full_train, test = train_test_split(full, test_size=0.2, random_state=SEED)
 
 model_args = T5Args()
-model_args.num_train_epochs = 5
+model_args.num_train_epochs = 10
 model_args.no_save = False
 model_args.flax_model = True
 model_args.fp16 = False
@@ -43,9 +43,9 @@ model_args.manual_seed = SEED
 model_args.early_stopping_patience = 25
 model_args.save_steps = 20000
 
-model_args.output_dir = os.path.join("outputs", "sint5_oscar")
-model_args.best_model_dir = os.path.join("outputs", "sint5_oscar", "best_model")
-model_args.cache_dir = os.path.join("cache_dir", "sint5_oscar")
+model_args.output_dir = os.path.join("outputs", "sint5_oscar_2")
+model_args.best_model_dir = os.path.join("outputs", "sint5_oscar_2", "best_model")
+model_args.cache_dir = os.path.join("cache_dir", "sint5_oscar_2")
 
 model_args.wandb_project = "NSINa Caption Generation"
 model_args.wandb_kwargs = {"name": model_name}
@@ -62,7 +62,7 @@ model = T5Model(model_type, model_args.best_model_dir, args=model_args, use_cuda
 preds = model.predict(input_list)
 
 test["predictions"] = preds
-test.to_csv(os.path.join("outputs", "sint5_oscar", "predictions.tsv"), sep='\t', encoding='utf-8', index=False)
+test.to_csv(os.path.join("outputs", "sint5_oscar_2", "predictions.tsv"), sep='\t', encoding='utf-8', index=False)
 
 del model
 
